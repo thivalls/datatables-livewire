ca7526fad6b79311bbab32b6dd6fc526e885a1c8
valls <olavalls@gmail.com>
2021-01-03 15:20:31 -0300

    Adding pagination and search live

--- app/Http/Livewire/Datatables.php
+++ app/Http/Livewire/Datatables.php
@@ -10,8 +10,10 @@ class Datatables extends Component
 {
     use WithPagination;
     
+    public $search;
+
     public function render()
     {
-        return view('livewire.datatables', ["users" => User::paginate(10)]);
+        return view('livewire.datatables', ["users" => User::where('name', 'like', "%$this->search%")->paginate()]);
     }
 }
--- resources/views/livewire/datatables.blade.php
+++ resources/views/livewire/datatables.blade.php
@@ -1,23 +1,25 @@
 <div>
     @if(count($users))
-    <div class="flex flex-col space-y-4 container mx-auto">
-        <div>
+    <div class="w-full max-w-5xl flex flex-col space-y-4 container mx-auto">
+        <div class="space-y-4">
+            <div class="w-1/4">
+                <input wire:model="search" class="border text-light text-grey-700 pl-2" placeholder="Search users...">
+            </div>
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                            <tr>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                #
-                            </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Name
-                            </th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            
+                    <table wire:loading.class.delay="opacity-40" class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    #
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
                                 @foreach($users as $user)
                                 <tr>
                                     <td class="px-6 py-4 whitespace-nowrap">
@@ -30,7 +32,7 @@
                                     </td>
                                 </tr>  
                                 @endforeach
-                                </tbody>
+                            </tbody>
                         </table>
                     </div>
                 </div>
