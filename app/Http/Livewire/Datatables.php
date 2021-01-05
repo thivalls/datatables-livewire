7b808b19ef1157d5624222189f6a827a77726fd1
valls <olavalls@gmail.com>
2021-01-05 00:00:11 -0300

    Adding multiple sorting columns

--- app/Http/Livewire/Datatables.php
+++ app/Http/Livewire/Datatables.php
@@ -10,21 +10,38 @@ class Datatables extends Component
 {
     use WithPagination;
     
+    public $sorts = [];
     public $filters = [
         'search' => null
     ];
+
+    public function sortBy ($column) {
+        if(! isset($this->sorts[$column])) return $this->sorts[$column] = 'asc';
+        if($this->sorts[$column] === 'asc') return $this->sorts[$column]  = 'desc';
+        unset($this->sorts[$column]);
+    }
+
+    public function applySorting($query) {
+        foreach($this->sorts as $field => $direction) {
+            $query->orderBy($field, $direction);
+        }
+        return $query;
+    }
 
     public function updatedFilters() {
         $this->resetPage();
     }
 
+    public function runProductFilter() {
+        $query = User::query()
+                ->when($this->filters['search'], fn($query, $search) => $query->where("name", "like", "%$search%" ));
+        return $this->applySorting($query);
+    }
+
     public function render()
     {
         return view('livewire.datatables', [
-            "users" => 
-                User::query()
-                ->when($this->filters['search'], fn($query, $search) => $query->where("name", "like", "%$search%" ))
-                ->paginate()]
+            "users" => $this->runProductFilter()->paginate() ]
         );
     }
 }
